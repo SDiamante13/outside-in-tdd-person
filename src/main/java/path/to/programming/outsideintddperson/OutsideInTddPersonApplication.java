b7771d5afe82daaab9fb473e22922bd4c5f328e2
Steven Diamante <StevenDiamante13@gmail.com>
2021-06-10 22:57:47 -0500

    Initial State: Blank Spring Boot Starter project

---
+++ src/main/java/path/to/programming/outsideintddperson/OutsideInTddPersonApplication.java
@@ -0,0 +1,13 @@
+package path.to.programming.outsideintddperson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutsideInTddPersonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutsideInTddPersonApplication.class, args);
+	}
+
+}
